23148104ed6bc7c0281436b0878fd5545c36c163
taosewu <xingtao668@gmail.com>
2025-09-23 15:19:20 +0800

    Update js.js

--- js.js
+++ js.js
@@ -37,7 +37,6 @@ var emails = [
 ];
 
 var urls=[
-	'1020154.xyz',
 	'1020155.xyz',
 	'1020156.xyz',
 	'1020157.xyz',
@@ -150,4 +149,5 @@ window.onload = function () {
     // Description
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
+
 }
