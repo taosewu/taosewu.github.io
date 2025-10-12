8862525b7c1228206d9fd246da87eaf4d1bf9677
taosewu <xingtao668@gmail.com>
2025-10-12 19:14:54 +0800

    Update js.js

--- js.js
+++ js.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'10201522.xyz',
+	'10201523.xyz',
 ];
 
 var newestUrls = [];
@@ -148,6 +148,7 @@ window.onload = function () {
     mainElem.appendChild(noticeFieldElem);
 
 }
+
 
 
 
